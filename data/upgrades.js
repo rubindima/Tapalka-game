b717b220805fb1a6ea1c7345ab90784a946187f8
Rubin07 <dimamamun07@gmail.com>
2025-05-19 10:51:41 +0300

    Create upgrades.js

---
+++ data/upgrades.js
@@ -0,0 +1,18 @@
+
+
+const upgrades = [
+  {
+    id: 1,
+    name: "Click Accelerator",
+    description: "Speed of earning x10",
+    price: 40000
+  },
+  {
+    id: 2,
+    name: "Coin Multiplier",
+    description: "Doubles all coin income",
+    price: 100000
+  }
+];
+
+module.exports = upgrades;
