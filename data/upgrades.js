deaaa9e116ab0c248ad4d994467c1a43bf1221c6
Rubin07 <dimamamun07@gmail.com>
2025-06-09 10:08:07 +0300

    Update upgrades.js

--- data/upgrades.js
+++ data/upgrades.js
@@ -1,18 +1,8 @@
 
-
 const upgrades = [
-  {
-    id: 1,
-    name: "Click Accelerator",
-    description: "Speed of earning x10",
-    price: 40000
-  },
-  {
-    id: 2,
-    name: "Coin Multiplier",
-    description: "Doubles all coin income",
-    price: 100000
-  }
+  { id: 1, name: "Click Accelerator", description: "speed x10", price: 40000 },
+  { id: 2, name: "Coin Multiplier", description: "income x2", price: 100000 }
 ];
 
 module.exports = upgrades;
+
